6c2615d2ee9357579dfc5674a0553af15e4d0505
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2025-02-21 15:40:49 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Owner: {{ $name }}</h3>
+    <img src="img/kayoko.jpg" alt="" class="w-64">
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl text-cyan-700">Axel Sean Cahyono Putra</h3>
+    <p class="text-lg text-cyan-500">Email: axelseancp@gmail.com</p>
+    <p class="text-lg text-cyan-500">085334473368</p>
+</x-layout>

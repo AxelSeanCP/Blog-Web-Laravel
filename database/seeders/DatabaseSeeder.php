e5b86bdb71ffeb78d48b4f6091b3deca96c4419d
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2025-02-22 18:17:03 +0700

    seeding database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,28 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Design',
+        //     'slug' => 'web-design',
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Mengapa meltryllis adalah best girl di FGO. pertama karena saya suka desain dari karakternya yang agak terbuka namun desain terbuka tersebut disesuaikan dengan sikapnya yang ingin menjadi sorot perhatian sehingga desain tersebut sangat cocok dengan kepribadian dia. Kedua karena dia adalah istri gweh.',
+        // ]);
+
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all(),
+        ])->create();
     }
 }

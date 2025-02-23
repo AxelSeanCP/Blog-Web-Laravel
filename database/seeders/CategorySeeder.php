96a0398867489eb910f9231da5d2f9a0c2149874
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2025-02-23 17:45:28 +0700

    redesign ui with flowbite

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,26 +18,31 @@ class CategorySeeder extends Seeder
         Category::create([
             'name' => 'Web Design',
             'slug' => 'web-design',
+            'color' => 'red'
         ]);
 
         Category::create([
             'name' => 'UI UX',
             'slug' => 'ui-ux',
+            'color' => 'green',
         ]);
 
         Category::create([
             'name' => 'Machine Learning',
             'slug' => 'machine-learning',
+            'color' => 'orange',
         ]);
 
         Category::create([
             'name' => 'Data Science',
             'slug' => 'data-science',
+            'color' => 'yellow',
         ]);
 
         Category::create([
             'name' => 'Cyber Security',
             'slug' => 'cyber-security',
+            'color' => 'blue',
         ]);
     }
 }
